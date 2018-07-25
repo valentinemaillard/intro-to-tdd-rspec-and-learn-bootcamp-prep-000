53014fcb35d7a71ba4f9555ed666c7fc91618b7e
valentine-m@live.fr <valentine-m@live.fr>
2018-07-25 17:03:33 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,5 @@
-def current_age_for_birth_year(1984)
-  puts current_age_for_birth_year(1984)
+def current_age_for_birth_year(birthday)
+ year=2003
+ year.to_i-birthday.to_i
 end
+puts current_age_for_birth_year(1984)
